62880df8a3e4b4e40b214589ae4965986479255b
Youtpout <EddyBoughioul@hotmail.fr>
2023-11-01 23:31:27 +0100

    private key from base58 is correct

--- MinaSignerNet/PrivateKey.cs
+++ MinaSignerNet/PrivateKey.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Globalization;
@@ -26,17 +27,27 @@ namespace MinaSignerNet
 
         public const byte VersionByte = 90;
 
+        // the modulus. called `p` in most of our code.
+        public BigInteger p = BigInteger.Parse("40000000000000000000000000000000224698fc094cf91b992d30ed00000001", NumberStyles.HexNumber);
+
+        public BigInteger q = BigInteger.Parse("40000000000000000000000000000000224698fc0994a8dd8c46eb2100000001", NumberStyles.HexNumber);
+
         public BigInteger S { get; set; }
 
         public PrivateKey(string base58)
         {
             List<byte> decode = base58.Decode().ToList();
 
+            BigInteger x = new BigInteger(decode.ToArray());
+
+            BigInteger y = mod(x, p);
+
+
             var checksum = new List<byte>(decode);
             checksum.RemoveRange(0, decode.Count - 5);
 
-            var originalBytes = new List<byte>(decode);
-            originalBytes.RemoveRange(decode.Count - 5, 4);
+            var originalBytes = base58.Decode().ToList();
+            originalBytes.RemoveRange(originalBytes.Count - 4, 4);
 
             Debug.WriteLine("original bytes 0 " + originalBytes[0]);
 
@@ -45,19 +56,9 @@ namespace MinaSignerNet
                 throw new Exception($"fromBase58Check: input version byte ${VersionByte} does not match encoded version byte ${originalBytes[0]}");
             }
 
-            decode.RemoveRange(0, 4);
+            originalBytes.RemoveAt(0);
 
-            var result = new List<byte>(base58.Decode());
-            result.RemoveAt(0);
-
-            string hexBytes = ByteArrayToString(result.ToArray()).Substring(0, 66);
-
-            List<byte> rebyte = base58.Decode().Skip(2).ToList();
-            rebyte.Reverse();
-
-            //Array.Reverse(res, 0, res.Length);
-
-            this.S = new BigInteger(rebyte.ToArray()); ;
+            this.S = BinableBigIntegerFromBytes(originalBytes.ToArray());
         }
 
         public static string ByteArrayToString(byte[] ba)
@@ -71,6 +72,28 @@ namespace MinaSignerNet
                              .Where(x => x % 2 == 0)
                              .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
                              .ToArray();
+        }
+
+        private BigInteger mod(BigInteger input, BigInteger mod)
+        {
+            BigInteger x = input % mod;
+            if (x < 0) return x + mod;
+            return x;
+        }
+
+        // https://github.com/o1-labs/o1js-bindings/blob/5e5befc8579393dadb96be1917642f860624ed07/lib/provable-bigint.ts#L62
+        private BigInteger BinableBigIntegerFromBytes(byte[] bytes)
+        {
+            int end = bytes.Length;
+            BigInteger x = BigInteger.Zero;
+            int bitPosition = 0;
+            // first byte is verif byte escape it
+            for (int i = 1; i < end; i++)
+            {
+                x += new BigInteger(bytes[i]) << bitPosition;
+                bitPosition += 8;
+            }
+            return x;
         }
 
     }
