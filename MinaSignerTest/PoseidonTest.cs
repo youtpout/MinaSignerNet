c2d583bdf6f56155c5858688ab9bf4eec5b91f68
Youtpout <EddyBoughioul@hotmail.fr>
2025-07-25 22:47:56 +0200

    update test

--- MinaSignerTest/PoseidonTest.cs
+++ MinaSignerTest/PoseidonTest.cs
@@ -36,11 +36,12 @@ namespace MinaSignerTest
         [Fact]
         public void HashTwo()
         {
-            BigInteger message = BigInteger.Parse("12");
-            var list = new List<BigInteger> { message };
+            BigInteger message = BigInteger.Parse("3412");
+            BigInteger message2 = BigInteger.Parse("548748548");
+            var list = new List<BigInteger> { message, message2 };
 
             BigInteger hash = PoseidonHash.Hash(list);
-            var expected = BigInteger.Parse("20307190475163560179843878304233687113040243867319358507811895775846718326775");
+            var expected = BigInteger.Parse("24245350037390325723675562428846509781869515058976947458013661211417354108422");
 
             Assert.Equal(expected, hash);
         }
