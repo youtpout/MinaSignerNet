38690b04e13db49ab05e3a0764b89cf44fe8b50b
eddy <eddyboughioul@hotmail.fr>
2025-08-04 11:15:04 +0200

    add new tests

--- MinaSignerTest/PoseidonTest.cs
+++ MinaSignerTest/PoseidonTest.cs
@@ -46,7 +46,52 @@ namespace MinaSignerTest
             Assert.Equal(expected, hash);
         }
 
+        [Fact]
+        public void HashNegative()
+        {
+            List<BigInteger> list = new List<BigInteger>
+{
+    BigInteger.Parse("-35545"),
+    BigInteger.Zero,
+    BigInteger.Parse("-7878454"),
+    BigInteger.Parse("45524"),
+    BigInteger.MinusOne
+};
 
+            BigInteger hash = PoseidonHash.Hash(list);
+            var expected = BigInteger.Parse("17944732201997716732580423582703197695777318095974149487644452711464169895343");
+
+            Assert.Equal(expected, hash);
+        }
+
+
+        [Fact]
+        public void HashMultiple()
+        {
+
+            List<BigInteger> list = new List<BigInteger>
+{
+    BigInteger.Parse("7263514276861361464633452875109919113182485937660059416421780822303488364810"),
+    BigInteger.Zero,
+    BigInteger.Parse("14045753958617862754670070034440311287432747428158303518301078328357441472472"),
+    BigInteger.One,
+    BigInteger.Zero,
+    BigInteger.Zero,
+    new BigInteger(1000000000),
+    BigInteger.Parse("23155254961521224263414982804952607540765699032359833230215492926554005931079"),
+    BigInteger.Zero,
+    new BigInteger(8000000000),
+    new BigInteger(1000000000),
+    BigInteger.Zero,
+    BigInteger.One
+};
+
+
+            BigInteger hash = PoseidonHash.Hash(list);
+            var expected = BigInteger.Parse("9477952037298579560464731632706228830146195233473478290570101327698566553181");
+
+            Assert.Equal(expected, hash);
+        }
 
     }
 }
